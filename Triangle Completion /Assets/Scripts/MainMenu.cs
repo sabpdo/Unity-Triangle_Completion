b9feb41c9bc3ab9560b52cb438571215468fa6a1
sabpdo <71912064+sabpdo@users.noreply.github.com>
2021-02-01 17:08:01 -0800

    Create MainMenu.cs

---
+++ Triangle Completion /Assets/Scripts/MainMenu.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    Slider progressBar;
+    public string sceneToLoad;
+    AsyncOperation loadingOperation;
+    public CanvasGroup canvasGroup;
+    
+
+    List<AsyncOperation> scenesToLoad = new List<AsyncOperation>();
+    // Start is called before the first frame update
+    public void Start()
+    {
+
+    }
+   
+
+    
+    public void StartGame()
+    {
+        StartCoroutine(StartLoad());
+        SceneManager.UnloadSceneAsync("WelcomeInstructions");
+    }
+
+    IEnumerator StartLoad()
+    {
+        loadingScreen.SetActive(true);
+        yield return StartCoroutine(FadeLoadingScreen(1, 1));
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneToLoad);
+        while(!operation.isDone)
+        {
+            yield return null;
+        }
+
+        yield return StartCoroutine(FadeLoadingScreen(0, 1));
+        loadingScreen.SetActive(false);
+    }
+
+    IEnumerator FadeLoadingScreen(float targetValue, float duration)
+    {
+        float startValue = canvasGroup.alpha;
+        float time = 0;
+
+        while (time<duration)
+
+        {
+            canvasGroup.alpha = Mathf.Lerp(startValue, targetValue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        canvasGroup.alpha = targetValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        progressBar.value = Mathf.Clamp01(loadingOperation.progress / 0.9f);
+        
+    }
+}
