2cfb13f2d2e273e9f8c83e8f8037a00b02cbb0f0
sabpdo <71912064+sabpdo@users.noreply.github.com>
2021-02-03 15:03:14 -0800

    Update ReverseNormals.cs

--- Triangle Completion /Assets/Scripts/ReverseNormals.cs
+++ Triangle Completion /Assets/Scripts/ReverseNormals.cs
@@ -3,36 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 [RequireComponent(typeof(MeshFilter))]
 
+//CODE to make sure that pattern of hedge walls is on both sides (reference: youtube)
+
 public class ReverseNormals : MonoBehaviour
 {
     void Start()
     {
         MeshFilter filter = GetComponent(typeof(MeshFilter)) as MeshFilter;
-    if (filter != null)
-    {
-        Mesh mesh = filter.mesh;
-
-        Vector3[] normals = mesh.normals;
-        for (int i = 0; i < normals.Length; i++)
-            normals[i] = -normals[i];
-        mesh.normals = normals;
-
-        for (int m = 0; m < mesh.subMeshCount; m++)
+        if (filter != null)
         {
-            int[] triangles = mesh.GetTriangles(m);
-            for (int i = 0; i < triangles.Length; i += 3)
+            Mesh mesh = filter.mesh;
+
+            Vector3[] normals = mesh.normals;
+            for (int i = 0; i < normals.Length; i++)
+                normals[i] = -normals[i];
+            mesh.normals = normals;
+
+            for (int m = 0; m < mesh.subMeshCount; m++)
             {
-                int temp = triangles[i + 0];
-                triangles[i + 0] = triangles[i + 1];
-                triangles[i + 1] = temp;
+                int[] triangles = mesh.GetTriangles(m);
+                for (int i = 0; i < triangles.Length; i += 3)
+                {
+                    int temp = triangles[i + 0];
+                    triangles[i + 0] = triangles[i + 1];
+                    triangles[i + 1] = temp;
+                }
+                mesh.SetTriangles(triangles, m);
             }
-            mesh.SetTriangles(triangles, m);
         }
+        this.GetComponent<MeshCollider>().sharedMesh = filter.mesh;
     }
-    
-    this.GetComponent<MeshCollider>().sharedMesh = filter.mesh;
-    
-}
 
     // Update is called once per frame
     void Update()
