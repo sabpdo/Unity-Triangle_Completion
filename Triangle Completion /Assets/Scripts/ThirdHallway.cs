8383e6f495664bc23bddc58291312c7f5a68e581
sabpdo <71912064+sabpdo@users.noreply.github.com>
2021-03-07 17:01:22 -0800

    Update ThirdHallway.cs

--- Triangle Completion /Assets/Scripts/ThirdHallway.cs
+++ Triangle Completion /Assets/Scripts/ThirdHallway.cs
@@ -15,15 +15,18 @@ public class ThirdHallway : MonoBehaviour
     public GameObject player;
 
     private bool ThirdPole = false;
-    private bool done = false;
+    public bool done = false;
     private bool left = true;
     private bool openField;
+    public bool wallSpawned = false;
 
     private int numSpace = 0;
 
-    private float yrotation;
+    public float yrotation = 0;
     private float playerPositionx;
     private float playerPositionz;
+
+    public TrialManager TM;
 
     // Start is called before the first frame update
     void Start()
@@ -32,9 +35,10 @@ public class ThirdHallway : MonoBehaviour
         startPosition = new Vector3(-58, 3, 55);*/
 
         //Get Components from Main Script
-        Main refScript = GetComponent<Main>();
-        left = GetComponent<Main>().left;
-        openField = GetComponent<Main>().openField;
+        GameObject player = GameObject.Find("Camera View");
+        TrialManager TM = player.GetComponent<TrialManager>();
+        left = TM.left;
+        openField = TM.openField;
     }
 
 
@@ -59,6 +63,7 @@ public class ThirdHallway : MonoBehaviour
         yrotation = transform.localEulerAngles.y;
         playerPositionx = transform.position.x;
         playerPositionz = transform.position.z;
+        
 
         //Create Walls Based off Rotation and Position
         if (openField == false)
@@ -71,14 +76,18 @@ public class ThirdHallway : MonoBehaviour
             GameObject newWallL = (GameObject)Instantiate(wall, posSecond, Quaternion.Euler(0f, yrotation, 90f));
             newWallL.tag = "wallL";
         }
+        wallSpawned = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Get left from Main Script and get info if OpenField or not from Main Script
-        left = GetComponent<Main>().left;
-        openField = GetComponent<Main>().openField;
+        GameObject player = GameObject.Find("Camera View");
+        TrialManager trialManager = player.GetComponent<TrialManager>();
+        TrialManager TM = trialManager;
+        left = TM.left;
+        openField = TM.openField;
 
         //Once Collided with ThirdPole, keep track of spaces
         if (ThirdPole == true)
@@ -92,6 +101,7 @@ public class ThirdHallway : MonoBehaviour
             {
                 firstSpace();
                 numSpace = 2;
+                wallSpawned = true;
             }
 
             if (numSpace >= 3)
@@ -105,76 +115,6 @@ public class ThirdHallway : MonoBehaviour
             }
         }
         
-
-        /* THIS CODE IS FOR INFINITE HALLWAY
-         * if (wallDone == true)
-        {
-            //Determine how far the player has moved since last terrain update
-            int xMove = (int)Mathf.Abs(player.transform.position.x - startPosition.x);
-            int zMove = (int)Mathf.Abs(player.transform.position.z - startPosition.z);
-
-            if (xMove >= planeSize || zMove >= planeSize) //If player has moved more than one plane size
-            {
-
-                int random = (int)(Mathf.Floor(Mathf.Sqrt((xMove * xMove) + (zMove * zMove)))); //distance player has traveled - using distance formula
-
-                if (left == true)
-                {
-                    //Create walls based off how far the player has traveled
-                    for (int x = 1; x < (random + 1); x++)
-                    {
-                        Vector3 posSecond = new Vector3(87 + (x * 5), 3, -90 + (-5 * x));
-                        GameObject newWall3 = (GameObject)Instantiate(wall, posSecond, Quaternion.Euler(0f, 135f, 90f));
-                        newWall3.tag = "wallL";
-
-                        if (Input.GetKeyDown(KeyCode.Y))
-                        {
-                            break;
-                        }
-                    }
-
-                    for (int z = 1; z < (random + 1); z++)
-                    {
-                        Vector3 posSecond3 = new Vector3(95 + (z * 5), 3, -90 + (-5 * z));
-                        GameObject newWall4 = (GameObject)Instantiate(wall, posSecond3, Quaternion.Euler(0f, 135f, 90f));
-                        newWall4.tag = "wallL";
-
-                        if (Input.GetKeyDown(KeyCode.Y))
-                        {
-                            break;
-                        }
-                    }
-                }
-                else if (left == false)
-                {
-                    //Create walls based off how far the player has traveled
-                    for (int x = 1; x < (random + 1); x++)
-                    {
-                        Vector3 posSecond = new Vector3(87 + (x * -5), 3, -90 + (-5 * x));
-                        GameObject newWall3 = (GameObject)Instantiate(wall, posSecond, Quaternion.Euler(0f, 135f, 90f));
-                        newWall3.tag = "wallR";
-        if (Input.GetKeyDown(KeyCode.Space))
-                        {
-                            break;
-                        }
-                    }
-
-                    for (int z = 1; z < (random + 1); z++)
-                    {
-                        Vector3 posSecond3 = new Vector3(95 + (z * -5), 3, -90 + (-5 * z));
-                        GameObject newWall4 = (GameObject)Instantiate(wall, posSecond3, Quaternion.Euler(0f, 135f, 90f));
-                        newWall4.tag = "wallR";
-
-                        if (Input.GetKeyDown(KeyCode.Space))
-                        {
-                            break;
-                        }
-                    }
-                }
-            }
-
-            }*/
-
 
         //When Done
         if (done == true)
