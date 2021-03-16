2872737048587f0c6d3e28bb8caecc68b752d758
sabpdo <71912064+sabpdo@users.noreply.github.com>
2021-03-16 15:31:53 -0700

    Update DataManager.cs

--- Triangle Completion /Assets/Scripts/DataManager.cs
+++ Triangle Completion /Assets/Scripts/DataManager.cs
@@ -6,7 +6,7 @@ using System.IO;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
-//Ask how to use FirstPersonController
+//Script to Manage+Export Data
 
 public class DataManager : MonoBehaviour
 {
@@ -46,26 +46,32 @@ public class DataManager : MonoBehaviour
     public string typeTriangle;
 
     public TrialManager TM;
+
     // Start is called before the first frame update
     void Start()
     {
         FILE_NAME =  "trial.csv";
+
+        //Get Component WallSpawned + Done from Other Script
         GameObject player = GameObject.Find("Camera View");
         ThirdHallway TH = player.GetComponent<ThirdHallway>();
         wallSpawned = TH.wallSpawned;
         done = TH.done;
 
+        //Make sure the triangle type matches the other script
         typeTriangle = TM.typeTriangle;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Record Data/Get the Angle the person turned when they first press the space bar
         if (Input.GetKeyDown(KeyCode.Space) && wallSpawned)
         {
             recordData();
         }
 
+        //Write to file the data when the trial is done
         if (Input.GetKeyDown(KeyCode.Space) && done)
         {
             trialData = GetDataString();
@@ -81,18 +87,41 @@ public class DataManager : MonoBehaviour
         playerRotAngle = player.transform.forward;
 
 
+        //Setting the respective correct angle 
         if (TM.current == TM.acute)
         {
-            correctAngle = 60;
+            if (TM.type== 202) //acute1 4,2,60
+            {
+                correctAngle = 90f;
+            }
+            else //4,6,60
+            {
+                correctAngle = 40.6546526707f;
+            }
         }
         else if (TM.current == TM.right)
         {
-            correctAngle = 90;
-           
+            //Using ArcTangent to find the correct turn angle
+            if (TM.type == 105) //right 1 2,6,90
+            {
+                correctAngle = Mathf.Atan2(2, 6) * (Mathf.PI / 180);
+            }
+            else //6,4,90
+            {
+                correctAngle = Mathf.Atan2(6, 4) * (Mathf.PI / 180);
+            }
+
         }
         else if (TM.current == TM.obtuse)
         {
-            correctAngle = 120;
+            if (TM.type == 305) //obtuse1 2,4,120
+            {
+                correctAngle = 15.89416531f;
+            }
+            else //6,2,120
+            {
+                correctAngle = 46.10211375f;
+            }
         }
 
     }
@@ -105,7 +134,44 @@ public class DataManager : MonoBehaviour
 
     public float GetInputAngle()
     {
-        float angle = GetComponent<ThirdHallway>().yrotation;
+        float wallAngle = GetComponent<ThirdHallway>().yrotation;
+        float angle=0;
+
+        //The angle the person turns is essentially the angle they were in the second hallway minus how much they turned (the absolutet
+        //value/magnitude)
+        if (TM.left)
+        {
+            if (TM.current == TM.acute)
+            {
+                angle = Mathf.Abs(270 - wallAngle);
+                
+            }
+            else if (TM.current == TM.right)
+            {
+                angle = Mathf.Abs(240 - wallAngle);
+
+            }
+            else if (TM.current == TM.obtuse)
+            {
+                angle = Mathf.Abs(300 - wallAngle);
+            }
+        }
+        else
+        {
+            if (TM.current == TM.acute)
+            {
+                angle = Mathf.Abs(90 - wallAngle);
+            }
+            else if (TM.current == TM.right)
+            {
+                angle = Mathf.Abs(120 - wallAngle);
+
+            }
+            else if (TM.current == TM.obtuse)
+            {
+                angle = Mathf.Abs(60 - wallAngle);
+            }
+        }
 
         return angle; 
     }
