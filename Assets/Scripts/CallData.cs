c399fb61325adeb4779d992652c232381a454dca
sabpdo <71912064+sabpdo@users.noreply.github.com>
2021-09-18 17:45:17 -0700

    Create CallData.cs

---
+++ Assets/Scripts/CallData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CallData : MonoBehaviour
+{
+    public float time = 0;
+    private bool safe = false;
+    private bool timeEnabled = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        timeEnabled = true;
+        
+       
+        if (timeEnabled == true)
+        {
+            time += Time.deltaTime;
+        }
+        if (time > 0.0999)
+        {
+            Data.MyTools1.DEV_AppendDefaultsToReport();
+            time = 0;
+        }
+    }
+}
