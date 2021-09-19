1407c978ac44757ba4d89c3d2657ac9a7573a0a7
sabpdo <71912064+sabpdo@users.noreply.github.com>
2021-09-18 17:45:20 -0700

    Create ChangeTexture.cs

---
+++ Assets/Scripts/ChangeTexture.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTexture : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject cube;
+
+    [SerializeField]
+    public Texture[] textures;
+    private Renderer cubeRenderer;
+    private int textureIndex;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeRenderer = cube.GetComponent<Renderer>();
+        textureIndex = Random.Range(0, textures.Length);
+        cubeRenderer.material.mainTexture = textures[textureIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
